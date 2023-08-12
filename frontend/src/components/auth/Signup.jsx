3c34876b2afbeb1f7dd246b43db703633c4fbf07
piumi <piyumikavindyappk@gmail.com>
2023-08-12 21:16:46 +0530

    update dark theme color

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Container from '../Container'
 import Title from '../form/Title'
 import FormInput from '../form/FormInput'
@@ -6,17 +6,43 @@ import Submit from '../form/Submit'
 import CustomLink from '../CustomLink'
 import { commonModalClasses } from '../../utils/theme'
 
+const validateUserInfo=({name, email, password})=>{
+if(!name.trim()) return {ok:false, error: 'Name is missing!'}
+if(/^[a-z A-Z]+$/.test(name)) return {ok:false, error: 'Invalid name!'}
+
+if(email.trim()) return {ok:false, error: 'emial is missing!'}
+}
+
 export default function Signup() {
-  return (
+
+  const [userInfo, setUserInfo]= useState({
+    name: '',
+    email: '',
+    password:'',
+  });
+const handleChange = ({target}) =>{
+  const {value, name}=target;
+  setUserInfo({...userInfo,[name]:value})
+ 
+};
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  console.log(userInfo);
+ 
+};
+
+  const{name,email,password}= userInfo
+    return (
     <div className="fixed inset-0 dark:bg-primary bg-green-200 -z-10 flex justify-center items-center p-1"> 
      
      
       <Container>
-        <form className={commonModalClasses+'w-72'}> 
+        <form onSubmit={handleSubmit} className={commonModalClasses+'w-72'}> 
          <Title Children='Sign up'> Sign up</Title>
-         <FormInput label='Name' placeholder='piumi kavindya' name='Name'/>
-         <FormInput label='Eamil' placeholder='abcd@gmail.com' name='email'/>
-         <FormInput label='Password' placeholder='***********' name='Password'/>
+         <FormInput value={name} onChange={handleChange} label='Name' placeholder='piumi kavindya' name='Name'/>
+         <FormInput value={email} onChange= {handleChange} label='Eamil' placeholder='abcd@gmail.com' name='email'/>
+         <FormInput value={password} onChange={handleChange}  label='Password' placeholder='***********' name='Password'
+         type='password'/>
          
          <Submit  value="Sign up"/>
 
