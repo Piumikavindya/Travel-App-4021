0b0f75e44f5860008922db55b9e360519afd020f
piumi <piyumikavindyappk@gmail.com>
2023-08-29 05:14:06 +0530

    edit Hbar in Home page

--- frontend/src/components/User/Hbar.jsx
+++ frontend/src/components/User/Hbar.jsx
@@ -8,8 +8,15 @@ export default function Hbar() {
             home
            
             </div>
-            <div className='overlay-image h-30'>
+            <div className='overlay-image p-2 h-30'>            
 
+            <img src="/logo2.png"  alt="" className="h-20 mx-auto "></img>
+                <h1 className=' Hbar-text text-center'>
+                    Create Your<br/>
+                       Own <br/>
+                      Journey
+                </h1>
+            <p className='text-white text-center'>Where Every Journey Becomes an Infinite Adventure.<br/>Explore the World Without Limits, with Travel Infinity</p>
 
             </div>
     </div>
